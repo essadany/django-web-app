e5be3ab1730c4a9662465e9ec21f503e35529a3f
essadany <essadanyya@cy-tech.fr>
2025-04-02 18:05:38 +0200

    connect bands detail page and band detail with database

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -20,8 +20,9 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
+    path('bands/', views.band_list, name='band-list'),
     path('about-us/', views.about),
     path('listings/', views.listings),
     path('contact/', views.contact),
+    path('bands/<int:id>/', views.band_detail, name='band-detail'),
 ]
