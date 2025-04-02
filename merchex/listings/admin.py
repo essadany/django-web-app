896a91de0f00e0beb4100230ae580776e89d8394
essadany <essadanyya@cy-tech.fr>
2025-04-02 17:21:26 +0200

    add Model Listing and link it with Band using foreing key

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Band
+from .models import Listing
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'genre', 'year_formed', 'active')
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'sold', 'year', 'type', 'band')
+    list_filter = ('band',)
+    search_fields = ('title', 'description')
+
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -20,5 +20,32 @@ class Band(models.Model):
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
     
-class Annonce(models.Model):
-    title = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.name}"
+    
+class Listing(models.Model):
+    class ListingType(models.TextChoices):
+        RECORDS = 'R'
+        MERCHANDISE = 'M'
+        POSTERS = 'P'
+        MISC = 'S'
+    
+    title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+        null=True,
+        validators=[MinValueValidator(1900), MaxValueValidator(2025)]
+    )
+    type = models.fields.CharField(
+        max_length=5,
+        choices=ListingType.choices,
+    )
+    band = models.ForeignKey(
+        Band,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    
+    def __str__(self):
+        return f"{self.band.name}"
